7851d112a7634eccf90c67eb34753a05963a7038
Ilya Pinyagin <ivp4797@mail.ru>
2019-07-16 18:55:53 +0700

    Add "Back" icon to Details screen

--- OrderDetails.js
+++ OrderDetails.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
 import OrderArea from "./OrderArea";
 import Status from "./Status";
 import { formattedDateTime } from "./dateTimeUtils";
@@ -9,7 +10,10 @@ export default class OrderDetails extends React.Component {
 		const order = this.props.navigation.getParam("order");
 		return (
 			<View style={{ backgroundColor: "#1159BE", height: "100%" }}>
-				<Text style={{ color: "white", fontWeight: "bold", fontSize: 25, paddingLeft: 15, marginTop: 30, marginBottom: 30 }}>Заказ { order.title }</Text>
+				<View style={{ display: "flex", flexDirection: "row", alignItems: "center", paddingLeft: 5, marginTop: 30, marginBottom: 30 }}>
+					<Icon name="navigate-before" color="white" size={ 40 } onPress={ () => this.props.navigation.goBack() } />
+					<Text style={{ color: "white", fontWeight: "bold", fontSize: 25, marginLeft: 15 }}>Заказ { order.title }</Text>
+				</View>
 				<OrderArea style={{ display: "flex", flexDirection: "column", marginLeft: 10, marginRight: 10 }}>
 					<View style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "flex-start", marginBottom: 40 }}>
 						<View style={{ display: "flex", flexDirection: "column" }}>
