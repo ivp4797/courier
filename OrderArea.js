bc8a15dd8d8bb9a4508752ea659d1a73c66a8d8a
Ilya Pinyagin <ivp4797@mail.ru>
2019-07-17 02:24:01 +0700

    OrderArea styles

--- OrderArea.js
+++ OrderArea.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: "white",
+		borderRadius: 10,
+		padding: 10,
+		marginTop: 10,
+		marginBottom: 10
+	}
+});
 
 export default class OrderArea extends React.Component {
 	render() {
 		const { style, ...other } = this.props;
 		return (
-			<View
-				style={{
-					backgroundColor: "white",
-					borderRadius: 10,
-					padding: 10,
-					marginTop: 10,
-					marginBottom: 10,
-					...style
-				}}
-				{ ...other }
-			>
+			<View style={[ styles.container, style ]} { ...other }>
 				{ this.props.children }
 			</View>
 		);
